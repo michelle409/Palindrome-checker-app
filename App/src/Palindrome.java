7d41e1dc4004f12288d73af2a652d36450a6eb5e
michelle409 <michellewilson0904@gmail.com>
2026-03-11 15:17:13 +0530

    UC8 commited

--- App/src/Palindrome.java
+++ App/src/Palindrome.java
@@ -4,18 +4,18 @@ public class Palindrome {
 
     public static void main(String[] args) {
 
-        String input = "refer";
+        String input = "level";
 
-        Deque<Character> deque = new ArrayDeque<>();
+        LinkedList<Character> list = new LinkedList<>();
 
         for (char c : input.toCharArray()) {
-            deque.add(c);
+            list.add(c);
         }
 
         boolean isPalindrome = true;
 
-        while (deque.size() > 1) {
-            if (deque.removeFirst() != deque.removeLast()) {
+        while (list.size() > 1) {
+            if (list.removeFirst() != list.removeLast()) {
                 isPalindrome = false;
                 break;
             }
